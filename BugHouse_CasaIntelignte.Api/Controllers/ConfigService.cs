745843bfd3e48401b039c1de8e97b60c7d3fddd8
Gilberto Augusto Magalhaes da Silva <gilberto@saurus.com.br>
2022-09-28 17:25:27 -0300

    first commit

---
+++ BugHouse_CasaIntelignte.Api/Controllers/ConfigService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugHouse_CasaIntelignte.Api.Controllers
+{
+    public class ConfigService : Controller
+    {
+
+
+        private string xPassOauth = "MyKeyApiEndPoitn_ArduinoIpSetings_aiofhsfiuohans8342or208930fni0vk";
+
+
+        [HttpGet("api/v1/GetIp")]
+        public IActionResult GetIp()
+        {
+            return Ok();
+        }
+
+
+        [HttpGet("api/v1/PostLastIp")]
+        public IActionResult PostLastIp(string Interface, string Oauth, string User)
+        {
+            if (Oauth != xPassOauth)
+                return Unauthorized();
+
+            var xIdInterface = Interface;
+            var xUsuario = User;
+
+
+           var xIpClient = this.HttpContext.Connection.RemoteIpAddress.ToString();
+
+
+            return Ok(xIpClient);
+        }
+
+    }
+}
